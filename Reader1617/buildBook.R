8afff3b89075b1219810678c86851870dfb6fd02
Fred Hasselman <namlessah.derf@gmail.com>
2016-11-22 15:02:26 +0100

    Upload Bookdown reader

---
+++ Reader1617/buildBook.R
@@ -0,0 +1,6 @@
+bookdown::gitbook(fig_caption = TRUE,
+        number_sections = FALSE,
+        self_contained = FALSE,
+        split_by = "rmd",
+        split_bib = FALSE,
+        config = list())
