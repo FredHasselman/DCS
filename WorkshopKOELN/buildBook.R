1e63531c1dca7d3f3faa869d215d20bb4d43dca9
Fred Hasselman <me@fredhasselman.com>
2017-05-11 23:51:15 +0200

    ws1

--- WorkshopKOELN/buildBook.R
+++
@@ -1,6 +0,0 @@
-bookdown::gitbook(fig_caption = TRUE,
-        number_sections = FALSE,
-        self_contained = FALSE,
-        split_by = "rmd",
-        split_bib = FALSE,
-        config = list())
